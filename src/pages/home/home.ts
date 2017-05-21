db7eed7e4d0eb996c85e277c643b7c0df7ceb5b8
infosyspranay <infosyspranay@ooyala.com>
2017-05-20 23:17:36 -0700

    Add an array of persons in homepage component, Add ion-item-sliding directive to the person list display on home page and add left and right side slide button options

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,12 +6,49 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-  public person = {
+  public people = [{
     "gender": "male",
     "name": {
       "title": "mr",
       "first": "romain",
       "last": "hoogmoed"
+    },
+    "location": {
+      "street": "1861 jan pieterszoon coenstraat",
+      "city": "maasdriel",
+      "state": "zeeland",
+      "postcode": 69217
+    },
+    "email": "romain.hoogmoed@example.com",
+    "login": {
+      "username": "lazyduck408",
+      "password": "jokers",
+      "salt": "UGtRFz4N",
+      "md5": "6d83a8c084731ee73eb5f9398b923183",
+      "sha1": "cb21097d8c430f2716538e365447910d90476f6e",
+      "sha256": "5a9b09c86195b8d8b01ee219d7d9794e2abb6641a2351850c49c309f1fc204a0"
+    },
+    "dob": "1983-07-14 07:29:45",
+    "registered": "2010-09-24 02:10:42",
+    "phone": "(123)-976-2433",
+    "cell": "(123)-247-2332",
+    "id": {
+      "name": "BSN",
+      "value": "04242023"
+    },
+    "picture": {
+      "large": "https://randomuser.me/api/portraits/men/83.jpg",
+      "medium": "https://randomuser.me/api/portraits/med/men/83.jpg",
+      "thumbnail": "https://randomuser.me/api/portraits/thumb/men/83.jpg"
+    },
+    "nat": "NL"
+  },
+  {
+    "gender": "male",
+    "name": {
+      "title": "mr",
+      "first": "sachin",
+      "last": "tendulkar"
     },
     "location": {
       "street": "1861 jan pieterszoon coenstraat",
@@ -37,12 +74,49 @@ export class HomePage {
       "value": "04242023"
     },
     "picture": {
-      "large": "https://randomuser.me/api/portraits/men/83.jpg",
-      "medium": "https://randomuser.me/api/portraits/med/men/83.jpg",
-      "thumbnail": "https://randomuser.me/api/portraits/thumb/men/83.jpg"
+      "large": "https://randomuser.me/api/portraits/men/85.jpg",
+      "medium": "https://randomuser.me/api/portraits/med/men/85.jpg",
+      "thumbnail": "https://randomuser.me/api/portraits/thumb/men/85.jpg"
     },
     "nat": "NL"
-  }
+  },
+  {
+    "gender": "male",
+    "name": {
+      "title": "mr",
+      "first": "rahul ",
+      "last": "dravid"
+    },
+    "location": {
+      "street": "1861 jan pieterszoon coenstraat",
+      "city": "maasdriel",
+      "state": "zeeland",
+      "postcode": 69217
+    },
+    "email": "romain.hoogmoed@example.com",
+    "login": {
+      "username": "lazyduck408",
+      "password": "jokers",
+      "salt": "UGtRFz4N",
+      "md5": "6d83a8c084731ee73eb5f9398b923183",
+      "sha1": "cb21097d8c430f2716538e365447910d90476f6e",
+      "sha256": "5a9b09c86195b8d8b01ee219d7d9794e2abb6641a2351850c49c309f1fc204a0"
+    },
+    "dob": "1983-07-14 07:29:45",
+    "registered": "2010-09-24 02:10:42",
+    "phone": "(233)-976-2323",
+    "cell": "(233)-323-3223",
+    "id": {
+      "name": "BSN",
+      "value": "04242023"
+    },
+    "picture": {
+      "large": "https://randomuser.me/api/portraits/men/86.jpg",
+      "medium": "https://randomuser.me/api/portraits/med/men/86.jpg",
+      "thumbnail": "https://randomuser.me/api/portraits/thumb/men/86.jpg"
+    },
+    "nat": "NL"
+  }]
   constructor(public navCtrl: NavController) {
 
   }
